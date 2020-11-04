a7ddbd8dce4b7c81be6ca70fa1b1a3e15b539215
Gaurav Gandhi <Gaurav.Gandhi@stonybrook.edu>
2020-11-04 09:13:44 +0530

    added userModel

--- src/model/UserModel.ts
+++ src/model/UserModel.ts
@@ -2,4 +2,20 @@ export type User = {
   id: string;
   username: string;
   password: string;
+  email: string;
+  profile: {
+    totalFollowers: number;
+    totalFollowing: number;
+    bio: string;
+    favGenre: Array<string>;
+    favArtist: Array<string>;
+    mixtapes: Array<object>;
+    followers: Array<string>;
+    following: Array<string>;
+    coins: number;
+    level: number;
+    xp: number;
+    tournamentsJoined: Array<string>;
+    tournamentsCreated: Array<string>;
+  }
 };
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -4,12 +4,16 @@ const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME || "localhost";
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
 
 const uri = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}`;
+console.log("HOSTNAME", MONGO_HOSTNAME);
 
+console.log("MONGOPORT", MONGO_PORT);
 export const client = new MongoClient(uri);
 export let db: Db;
 
+
 export async function initDb() {
   try {
+	  // setTimeout(() => console.log("start ya mf"), 1500);
     await client.connect();
     db = client.db("mujik");
     console.info("Successfully connected to DB.");
