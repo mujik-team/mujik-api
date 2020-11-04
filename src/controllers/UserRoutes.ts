f1f4beb2fbc17e7c0c7f68944067062dbb605eca
Gaurav Gandhi <Gaurav.Gandhi@stonybrook.edu>
2020-11-04 10:38:45 +0530

    working CRUD operations on user

--- src/controllers/UserRoutes.ts
+++ src/controllers/UserRoutes.ts
@@ -2,32 +2,68 @@ import { Route } from "./_types";
 import * as UserService from "../services/UserService";
 import { ResultError, ResultOK } from "../utils/ResultGenerator";
 
+/**
+ * 1. post = register a user
+ * 2. get = get a user - done
+ * 3. get = get a user profile - not sure
+ * 4. put = update a user - done
+ * 5. delete = delete a user
+ */
+
+
 export const UserRoutes: Route[] = [
   /**
    * Get a user by username.
    */
+  // {
+  //   path: "/user/:username",
+  //   method: "get",
+  //   handler: async (req, res) => {
+  //     console.log(req.params);
+  //     const { username } = req.params;
+  //     const user = await UserService.GetByUsername(username);
+
+  //     if (user) {
+  //       res.json(ResultOK(`Retrieved user ${username}.`, { user }));
+  //     } else {
+  //       res.status(400).json(ResultError("Error retrieving user."));
+  //     }
+  //   },
+  // },
+  /**
+   * Get a user by id
+   */
   {
-    path: "/user/:username",
+    path: "/user/:id",
     method: "get",
     handler: async (req, res) => {
-      const { username } = req.params;
-      const user = await UserService.GetByUsername(username);
+      const  { id }  = req.params;
+      const user = await UserService.GetUser(id);
 
       if (user) {
-        res.json(ResultOK(`Retrieved user ${username}.`, { user }));
+        res.json(ResultOK(`Retrieved user ${user.username}`, { user }));
       } else {
         res.status(400).json(ResultError("Error retrieving user."));
       }
-    },
+    }
   },
-
   /**
    * Create a new user.
    */
   {
     path: "/user/",
     method: "post",
-    handler: (req, res) => {},
+    handler: async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const newUser = await UserService.CreateUser(user);
+
+      if (newUser) {
+        res.json(ResultOK(`Created user ${user}.`, { user }));
+      } else {
+        res.status(400).json(ResultError("Error creating user."));
+      }
+    },
   },
   /**
    * Update the details of a user.
@@ -35,8 +71,16 @@ export const UserRoutes: Route[] = [
   {
     path: "/user/:id",
     method: "put",
-    handler: (req, res) => {
-      throw new Error("Not yet implemented.");
+    handler: async (req, res) => {
+      const { id } = req.params;
+      const user = req.body;
+      const updatedUser = await UserService.UpdateUser(id, user);
+      
+      if (updatedUser) {
+        res.json(ResultOK(`Updated user ${updatedUser.username}`, { updatedUser }));
+      } else {
+        res.status(400).json(ResultError("Error updating user"));
+      }
     },
   },
 
@@ -46,8 +90,15 @@ export const UserRoutes: Route[] = [
   {
     path: "/user/:id",
     method: "delete",
-    handler: (req, res) => {
-      throw new Error("Not yet implemented.");
+    handler: async (req, res) => {
+      const { id } = req.params;
+      const deletdUser = await UserService.DeleteUser(id);
+
+      if (deletdUser) {
+        res.json(ResultOK(`Deleted user ${deletdUser.username}`));
+      } else {
+        res.status(400).json(ResultError("Error deleting user"));
+      }
     },
   },
 ];
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,16 +1,35 @@
 import { User } from "../model/UserModel";
 import { db } from "../utils/db";
+const mongo = require("mongodb");
 
-export async function GetUser(id: string): Promise<User> {
+export async function GetUser(id: string): Promise<any> {
   const users = db.collection("user");
-
-  return { id: "12345", username: "mckillagorilla", password: "secret" };
+  const user = await users.findOne({ "_id": mongo.ObjectID(id) });
+  return user;
 }
 
-export async function GetByUsername(username: string): Promise<User | null> {
-  return { id: "12345", username: "mckillagorilla", password: "secret" };
+export async function GetByUsername(username: string): Promise<any | null> {
+  // return { id: "12345", username: "mckillagorilla", password: "secret" };
+  const users = db.collection("user");
+  const user = await users.findOne({ username: username });
+  return user;
 }
 
-export async function CreateUser(user: User): Promise<User> {
-  return { id: "1234", username: "mckillagorilla", password: "secret" };
+export async function CreateUser(user: User): Promise<any> {
+  // return { id: "1234", username: "mckillagorilla", password: "secret" };
+  const users = db.collection("user");
+  const newUser = await users.insertOne(user);
+  return newUser;
+}
+
+export async function DeleteUser(id: string): Promise<any> {
+  const users = db.collection("user");
+  const user = await users.deleteOne({ "_id": mongo.ObjectID(id) });
+  return user;
+}
+
+export async function UpdateUser(id: string, user: User): Promise<any> {
+  const users = db.collection("user");
+  const updatedUser = await users.replaceOne({ "_id": mongo.ObjectID(id) }, user);
+  return updatedUser;
 }
